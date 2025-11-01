eaceca4bafd2d5cd215a47e09e52e2c1a568c559
KiptooStephen <kiptoos@gmail.com>
2025-11-01 21:05:43 +0300

    Update views.py

--- alx_travel_app/listings/views.py
+++ alx_travel_app/listings/views.py
@@ -4,14 +4,19 @@ from .models import Booking
 from .serializers import BookingSerializer
 from .tasks import send_booking_confirmation_email
 
+
 class BookingViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
 
     def perform_create(self, serializer):
         booking = serializer.save()
-        # Trigger Celery email task
-        if booking.user and booking.user.email:
-            send_booking_confirmation_email.delay(
-                booking.user.email, booking.id
-            )
+        # pick email from user or booking
+        email = None
+        if hasattr(booking, "user") and booking.user and booking.user.email:
+            email = booking.user.email
+        elif hasattr(booking, "customer_email"):
+            email = booking.customer_email
+
+        if email:
+            send_booking_confirmation_email.delay(email, booking.id)
